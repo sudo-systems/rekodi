3ddce6db232015ed28b332410414a9656c0d2600
Sudo Systems <bram@sudo-systems.com>
2015-07-15 21:47:46 +0200

    - Attempting to keep kodi connection alive (check with interval)

--- public/js/player.js
+++ public/js/player.js
@@ -1,9 +1,10 @@
 var remote = require('remote');
-var BrowserWindow = remote.require('browser-window');
+var mainWindow = remote.getCurrentWindow();
+var player = null;
 var fs = require('fs');
 var path = require('path');
 var xec = require('xbmc-event-client');
-var mainWindow = remote.getCurrentWindow();
+var volumeSlider = null;
 
 var opts = {
   log: false,
@@ -21,56 +22,80 @@ kodiEventClient.connect(function(errors, bytes) {
   }
 });
 
-$(document).ready(function() {
-  $('[title]').jBox('Tooltip',  {
-    animation: 'move'
-  });
-  
-  $('#player [data-partial]').each(function() {
-    var loThis = $(this);
-    var templatePartial = 'partials/'+$(this).attr('data-partial')+'.html';
-    
-    $.ajax({
-      url: templatePartial,
-      success: function(data) {
-        loThis.html(data);
-      },
-      error: function(error) {
-        console.error(error);
-      }
+var Player = function() {
+  this.initTooltips = function() {
+    $('[title]').jBox('Tooltip',  {
+      animation: 'move'
     });
-  });
+  };
   
-  $('#player #topBar .windowControls .button.close').on('click', function() {
-    mainWindow.close();
-  });
+  this.loadPartials = function() {
+    $('#player [data-partial]').each(function() {
+      var loThis = $(this);
+      var templatePartial = 'partials/'+$(this).attr('data-partial')+'.html';
+
+      $.ajax({
+        url: templatePartial,
+        success: function(data) {
+          loThis.html(data);
+        },
+        error: function(error) {
+          console.error(error);
+        }
+      });
+    });
+  };
   
-  $('#player #topBar .windowControls .button.minimize').on('click', function() {
-    mainWindow.minimize();
-  });
+  this.initWindowControls = function() {
+    $('#player #topBar .windowControls .button.close').on('click', function() {
+      mainWindow.close();
+    });
 
-  $('#player #controls li').on('click', function() {
-    kodiEventClient.remotePress($(this).attr('data-button'));
-  });
-
-  var magicLine = $('#player nav .magic-line');
-  magicLine.css('left', $('#player nav li.active').position().left).data('origLeft', magicLine.position().left).data('origWidth', magicLine.width());
-
-  $('#tabsWrapper nav li').on('click', function(e) {
-    e.preventDefault();
+    $('#player #topBar .windowControls .button.minimize').on('click', function() {
+      mainWindow.minimize();
+    });
+  };
+  
+  this.initPlaybackControls = function() {
+    $('#player #controls li').on('click', function() {
+      kodiEventClient.remotePress($(this).attr('data-button'));
+    });
+  };
+  
+  this.initVisualElements = function() {
+    volumeSlider = new Dragdealer('volumeSlider');
     
-    $('#tabsWrapper nav li').removeClass('active');
-    $(this).addClass('active');
-    
-    var index = $(this).index();
-    $('#tabsWrapper .tabs').attr('data-tab', index);
+    var magicLine = $('#player nav .magic-line');
+    magicLine.css('left', $('#player nav li.active').position().left).data('origLeft', magicLine.position().left).data('origWidth', magicLine.width());
 
-    var leftPos = $(this).position().left;
-    var newWidth = $(this).width();
-    
-    magicLine.stop().animate({
-      left: leftPos,
-      width: newWidth
-    }, 500);
-  });
+    $('#tabsWrapper nav li').on('click', function(e) {
+      e.preventDefault();
+
+      $('#tabsWrapper nav li').removeClass('active');
+      $(this).addClass('active');
+
+      var index = $(this).index();
+      $('#tabsWrapper .tabs').attr('data-tab', index);
+
+      var leftPos = $(this).position().left;
+      var newWidth = $(this).width();
+
+      magicLine.stop().animate({
+        left: leftPos,
+        width: newWidth
+      }, 500);
+    });
+  };
+};
+
+function init() {
+  player = new Player();
+  player.initVisualElements();
+  player.initTooltips();
+  player.loadPartials();
+  player.initPlaybackControls();
+}
+
+$(document).ready(function() {
+  init();
 });
