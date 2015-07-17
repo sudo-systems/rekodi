f310303cd325094fcd2837402d0b2fa6e146de94
Sudo Systems <bram@sudo-systems.com>
2015-07-17 16:55:28 +0200

    - Got tooltips working again

--- app/scripts/app/config.js
+++ app/scripts/app/config.js
@@ -1,11 +1,4 @@
 angular.module('rekodiApp')
   .config([function() {
-      
-    $(document).ready(function() {
-      console.log('test');
-      
-      $('[title]').jBox('Tooltip',  {
-        animation: 'move'
-      });
-    });
+  
   }]);
