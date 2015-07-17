9f48fb0acb1d4e25c9e221e841543cca0fd2b5f0
Sudo Systems <bram@sudo-systems.com>
2015-07-17 15:57:39 +0200

    - Got jquery working - Added directive for tabs

--- app/scripts/app/controllers/playback_controls.js
+++ app/scripts/app/controllers/playback_controls.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('rekodiApp')
   .controller('rkPlaybackControlsCtrl', [
     '$scope', 
--- app/scripts/app/controllers/tabs.js
+++ app/scripts/app/controllers/tabs.js
@@ -1,5 +1,3 @@
-'use strict';
-
 angular.module('rekodiApp')
   .controller('rkTabsCtrl', [
     '$scope', 
