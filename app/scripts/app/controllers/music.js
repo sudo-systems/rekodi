c57b5ecb10949337f3debf61bdceb3f6394e1746
Sudo Systems <bram@sudo-systems.com>
2015-07-18 20:25:27 +0200

    - Music files view partially working

--- app/scripts/app/controllers/music.js
+++ app/scripts/app/controllers/music.js
@@ -2,33 +2,74 @@ rekodiApp.controller('rkMusicCtrl', ['$scope', '$timeout', 'rkKodiWsApiService',
   function($scope, $timeout, rkKodiWsApiService) {
     $scope.libraryData = {};
     $scope.filesData = {};
-    $scope.sourcesData = {};
+    var kodiWsApiConnection = null;
     
     $scope.getLibraryData = function() {
       
     };
     
     $scope.getSourcesData = function() {
-      if(rkKodiWsApiService.isConnected()) {
-        var kodiWsApi = rkKodiWsApiService.getConnection();
-        var sourcesPromise = kodiWsApi.Files.GetSources({
-          media: 'music'
+        kodiWsApiConnection = rkKodiWsApiService.getConnection();
+        var promise = kodiWsApiConnection.Files.GetSources({
+          media: 'music',
+          sort: {
+            order: 'ascending',
+            method: 'label'
+          }
         });
         
-        sourcesPromise.then(function(data) {
-          $scope.sourcesData = data;
-          console.dir($scope.sourcesData);
-        }, function(error) {
-        });
-      }
+        promise.then(function(data) {
+          $scope.filesData = data.sources;
+          $scope.$apply();
+        }, function(error) {});
     };
     
-    $scope.getFilesData = function() {
+    $scope.getDirectoryData = function(directory) {
+      kodiWsApiConnection = rkKodiWsApiService.getConnection();
+      var promise = kodiWsApiConnection.Files.GetDirectory({
+        directory: directory,
+        media: 'music',
+        sort: {
+            order: 'ascending',
+            method: 'label'
+          }
+      });
       
+      var dirWithoutTrailingSlash = directory.substr(0, directory.lastIndexOf('/'));
+      var dirUp = dirWithoutTrailingSlash.substr(0, dirWithoutTrailingSlash.lastIndexOf('/'))+'/';
+
+      promise.then(function(data) {
+        $scope.filesData = data.files;
+        $scope.filesData.unshift({
+          label: '..',
+          filetype: 'directory',
+          file: dirUp
+        });
+        $scope.$apply();
+      }, function(error) {
+        $scope.getSourcesData();
+      });
+    };
+    
+    $scope.getData = function() {
+      $scope.getSourcesData();
     };
     
     $scope.$root.$on('rkWsConnectionStatusChange', function(event, data) {
-      $scope.getSourcesData();
+      if(data.connected) {
+        $scope.getSourcesData();
+      }
+      else {
+        $scope.sourcesData = {};
+      }
+    });
+    
+    $scope.$evalAsync(function() { 
+      if(rkKodiWsApiService.isConnected()) {
+        $timeout(function() {
+          $scope.getSourcesData();
+        });
+      }
     });
   }
 ]);
